057870d6f806803ddded22b9ad82ebdf39d5155b
xiaox232 <xiaox232@morris.umn.edu>
2017-10-19 15:06:58 -0500

    Finished working on 'file_disemvowel.c'. Commented out 'skip' in 'file_disemvowel_test.bats'. All tests are running.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,45 +1,64 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BUF_SIZE 1024
 
-bool is_vowel (char x) { 
- 	if (x=='a' || x=='e' || x=='i' || x=='o' || x=='u' || x=='A' || x=='E' || x=='I' || x=='O' || x=='U') {
-		return true;
-	}
-  else {
-  return false;
-  }
+bool is_vowel(char c) { 
+   return strchr("aeiouAEIOU", (int) c) != NULL; 
 }
 
-int copy_non_vowels(int num_chars, char* in_buf, char* out_buf){
-	int letter = 0;
-	for (int i = 0; i < num_chars; i++){
- 		if (is_vowel (int_buf[i])) {
-			out_buf[letter] = in_buf[i];
-			letter++;	
-		}
- 	}
-	return letter;	
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+    int out_buf_index = 0;
+    int i;
+    for (i = 0; i < num_chars; i++) {
+        if (!is_vowel(in_buf[i])) {
+            out_buf[out_buf_index] = in_buf[i];
+            out_buf_index++;
+        }
+    }
+
+
+    return out_buf_index;
 }
 
 void disemvowel(FILE* inputFile, FILE* outputFile) { 
-    /*
-     * Copy all the non-vowels from inputFile to outputFile.
-     * Create input and output buffers, and use fread() to repeatedly read
-     * in a buffer of data, copy the non-vowels to the output buffer, and
-     * use fwrite to write that out. 
-     */
+    char *inBuffer = (char*) calloc(BUF_SIZE + 1, sizeof(char));
+    char *outBuffer = (char*) calloc(BUF_SIZE, sizeof(char));
+    int num_read = 0;
+    int num_non_vowels;
+
+    do {
+        num_read = fread(inBuffer, sizeof(char), BUF_SIZE, inputFile);
+        num_non_vowels = copy_non_vowels(num_read, inBuffer, outBuffer);
+        fwrite(outBuffer, sizeof(char), num_non_vowels, outputFile);
+    } while (num_read != 0);
+
+
+    free(inBuffer);
+    free(outBuffer);
 }
 
 int main(int argc, char *argv[]) { 
     FILE *inputFile; 
     FILE *outputFile;
 
-    // Code that processes the command line arguments 
-    // and sets up inputFile and outputFile.
+    inputFile = stdin;
+    outputFile = stdout;
+    
+    if (argc >= 2) {
+        inputFile = fopen(argv[1], "r");
+    }
+
+    if (argc >= 3) {
+        outputFile = fopen(argv[2], "w");
+    }
 
     disemvowel(inputFile, outputFile);
 
+    fclose(inputFile);
+    fclose(outputFile);
+    
     return 0; 
 }
