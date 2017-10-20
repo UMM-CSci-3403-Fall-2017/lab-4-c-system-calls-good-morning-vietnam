9ed05c56f12a828d54b72608a9c2e49b5bfc3840
Danish Malik <malik083@morris.umn.edu>
2017-10-20 02:41:49 -0500

    Fixed memory leaks in 'summarize_tree.c' and 'summarize_tree_ftw.c'.

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -10,8 +10,17 @@
 static int num_dirs, num_regular;
 
 bool is_dir(const char* path) {
-   struct stat buff;
-   return stat(path, &buff) != -1 && S_ISDIR(buff.st_mode);
+  struct stat buf;
+  int s = stat(path, &buf);
+
+  return (s == 0 && S_ISDIR(buf.st_mode));
+  /*
+   * Use the stat() function (try "man 2 stat") to determine if the file
+   * referenced by path is a directory or not.  Call stat, and then use
+   * S_ISDIR to see if the file is a directory. Make sure you check the
+   * return value from stat in case there is a problem, e.g., maybe the
+   * the file doesn't actually exist.
+   */
 }
 
 /* 
@@ -21,33 +30,49 @@ bool is_dir(const char* path) {
 void process_path(const char*);
 
 void process_directory(const char* path) {
-   DIR * dir = opendir(path);
-   
-   chdir(path);
-   struct dirent * dp;
-   while ((dp = readdir(dir)) != NULL) {
-       if(strcmp(dp->d_name, ".") != 0 &&
-            strcmp(dp->d_name, "..") != 0)
-       {
-            //printf("%s\n", dp -> d_name);
-            process_path(dp -> d_name);
-       }
-   }
-   
-   num_dirs++;
-   if(strcmp(".", path) != 0)
-        chdir("..");
-   closedir(dir);
+  num_dirs++;
+
+  DIR *dir;
+  struct dirent *dp;
+  dir = opendir(path);
+
+chdir(path);
+
+  while((dp = readdir(dir)) != NULL) {
+    if (strcmp(dp->d_name, ".") && strcmp(dp->d_name, "..")) {
+      process_path(dp->d_name);
+    }
+  }
+
+  closedir(dir);
+  chdir("..");
+  /*
+   * Update the number of directories seen, use opendir() to open the
+   * directory, and then use readdir() to loop through the entries
+   * and process them. You have to be careful not to process the
+   * "." and ".." directory entries, or you'll end up spinning in
+   * (infinite) loops. Also make sure you closedir() when you're done.
+   *
+   * You'll also want to use chdir() to move into this new directory,
+   * with a matching call to chdir() to move back out of it when you're
+   * done.
+   */
 }
 
 void process_file(const char* path) {
-   num_regular++;
+
+  num_regular++;
+
+  /*
+   * Update the number of regular files.
+   */
 }
 
 void process_path(const char* path) {
   if (is_dir(path)) {
     process_directory(path);
-  } else {
+
+ } else {
     process_file(path);
   }
 }
@@ -56,7 +81,7 @@ int main (int argc, char *argv[]) {
   // Ensure an argument was provided.
   if (argc != 2) {
     printf ("Usage: %s <path>\n", argv[0]);
-    printf ("where <path> is the file or root of the tree you want to summarize.\n");
+    printf ("       where <path> is the file or root of the tree you want to summarize.\n");
     return 1;
   }
 
@@ -67,6 +92,8 @@ int main (int argc, char *argv[]) {
 
   printf("There were %d directories.\n", num_dirs);
   printf("There were %d regular files.\n", num_regular);
-  
+
   return 0;
 }
+                      
+
--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <string.h>
 #include <ftw.h>
-
-
-static int num_dirs, num_regular;
-
-static int callback(const char *fpath, const struct stat *sb, int typeflag) {
-    // if the typeflag equals FTW_F then increments the number of regualr files by 1
-    // if the typrflag equals FTW_D then increments the number of directories by 1
-    if (typeflag == FTW_F){
-      num_regular++;
-    }
-    else if (typeflag == FTW_D){
-      num_dirs++;
-
-    }
-    //function need to return an integer
-    return 0;
-}
 
 #define MAX_FTW_DEPTH 16
 
+static int num_dirs = 0;
+static int  num_files = 0;
+
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+    if (typeflag == FTW_F) {
+        num_files++;
+        return 0;
+    } else if (typeflag == FTW_D) {
+        num_dirs++;
+        return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char** argv) {
     // Check arguments and set things up
-    if (argc != 2) {
-      printf ("Usage: %s <path>\n", argv[0]);
-      printf ("where <path> is the file or root of the tree you want to summarize.\n");
-      return 1;
-    }
 
+    if (argc != 2) {
+        printf ("Usage: %s <path>\n", argv[0]);
+        printf ("       where <path> is the file or root of the tree you want to summarize.\n");
+        return 1;
+    }
     ftw(argv[1], callback, MAX_FTW_DEPTH);
 
-    // Print out the results
+    // Print out the results 
+
     printf("There were %d directories.\n", num_dirs);
-    printf("There were %d regular files.\n", num_regular);
+    printf("There were %d regular files.\n", num_files);
+    
+    return 0;
 }
